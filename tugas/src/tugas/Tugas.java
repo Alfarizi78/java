f46f6a4b14fe7254470b51da9b5bbc8e8e7651c2
Muhamad Alfarizi <145635975+Alfarizi78@users.noreply.github.com>
2024-08-22 12:51:34 +0700

    Add files via upload

---
+++ tugas/src/tugas/Tugas.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas;
+
+import tugas.modul1.contoh_1;
+import tugas.modul1.latihan_1;
+import tugas.modul2.contoh_2;
+import tugas.modul2.latihan_2;
+import tugas.modul3.contoh_3;
+import tugas.modul3.latihan_3;
+import tugas.modul4.contoh_4;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Tugas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        contoh_4 modul4 = new contoh_4();
+        
+        modul4.setVisible(true);
+    }
+    
+}
